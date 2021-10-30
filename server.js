3da03c84426a904a5a4aebfa8442310194f5019a
Anis Anis <nissourock@gmail.com>
2021-10-30 10:50:15 +0100

    dffdfdfdfdfdf

--- server.js
+++ server.js
@@ -9,10 +9,8 @@ require("dotenv").config();
 app.use(express.json());
 app.use(cors());
 app.use(express.static(path.join(__dirname, "client", "build")))
-mongoose.connect("mongodb://127.0.0.1:27017/mern-todo", {
-    useNewUrlParser: true,
-    useUnifiedTopology: true
-}).then(() => console.log("connected to DB")).catch(console.error);
+mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true });
+;
 
 const Todo = require('./models/Todo');
 
@@ -53,4 +51,4 @@ app.get("*", (req, res) => {
     res.sendFile(path.join(__dirname, "client", "build", "index.html"));
 });
 
-app.listen(3001, () => console.log("server started on 3001"));
+app.listen(port, () => console.log("server started on 3001"));
